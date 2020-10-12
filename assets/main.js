7643817602e9cab52f5004df6464417ff65d5c31
Damian <damiianalmada06@gmail.com>
2020-10-12 09:34:45 -0300

    notas

--- assets/main.js
+++ assets/main.js
@@ -10,19 +10,6 @@ let formTasks = document.querySelector("#formTasks");
 
 let nombreSaved = "";
 
-nombreSaved = localStorage.getItem("nombreA");
-
-/*es la presentacion del div tareas BIENVENIDOS NOMBRE*/
-if(nombreSaved != null){
-    divPresentation.style.display = "none";
-    divTasks.style.display = "grid";
-    let nombrePresentation = `Bienvenido ${nombreSaved.toUpperCase()}`;
-    titleTasks.append(nombrePresentation);
-}else{
-    console.log("no hay nada en el localStorage");
-};
-
-
 /*es para guardar el nombre para la bienvenida*/
 formNombre.addEventListener("submit", function(){
     //localStorage.setItem("nombre", nombre);
@@ -36,19 +23,40 @@ formNombre.addEventListener("submit", function(){
     };
 });
 
+nombreSaved = localStorage.getItem("nombreA");
+console.log(nombreSaved)
+
+/*es la presentacion del div tareas BIENVENIDOS NOMBRE*/
+if(nombreSaved != null){
+    divPresentation.style.display = "none";
+    divTasks.style.display = "grid";
+    let nombrePresentation = `Bienvenido ${nombreSaved.toUpperCase()}`;
+    titleTasks.append(nombrePresentation);
+}else{
+    console.log("no hay nada en el localStorage");
+};
+
 
 /*es para guardar las tareas*/
 let tasks = [];
 formTasks.addEventListener("submit", function(){ 
-    if (localStorage.getItem("tasks")===null){
-        tasks.push(inputTasks.value);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-        inputTasks.value = "";
+    if(inputTasks.value == ""){
+        alert("Ingresa una Tarea Valida pelotudo");
     }else{
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-        tasks.push(inputTasks.value);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    };  
+        if (localStorage.getItem("tasks")===null){
+            tasks.push(inputTasks.value);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            inputTasks.value = "";
+            inputTasks.focus();
+        }else{
+            tasks = JSON.parse(localStorage.getItem("tasks"));
+            //se trae del localstorage para volver a guardar u posteriormente volver a subir
+            tasks.push(inputTasks.value);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            inputTasks.focus();
+        }; 
+    }
+     
 });
 
 
@@ -60,17 +68,19 @@ function getTasks(){
     listTask.innerHTML = "";
     for(let i = 0; i < tasks.length; i++){
         let title = tasks[i];
+        console.log(i)
         listTask.innerHTML += `<li>
-            <p class="title">${title}</p><button class="btn-red" onclick="through('${title}')">X</button>
+            <p id="title${i}">${title}</p><button class="btn-red" onclick="deleteForm('${title}')">X</button><button class="btn-red" onclick="through('${i}')">*</button>
         </li>
         `;
-        
     };
+    inputTasks.focus();
+    //localStorage.getItem("through");
 };
 
 getTasks();
 
-function through(ii){
+function deleteForm(ii){
     let tasks = JSON.parse(localStorage.getItem("tasks"));
     for(let i = 0; i < tasks.length; i++){
         if(tasks[i] == ii){
@@ -88,8 +98,50 @@ console.log(btnUsuario);
 btnUsuario.addEventListener("click", function(){
     localStorage.clear();
     location.reload();
-})
+});
 
+
+function through(ii){
+    /*let tasks = JSON.parse(localStorage.getItem(`tasks`));
+    let sub;
+    sub = tasks[ii];
+    console.log(sub);*/
+    //let p = document.createElement('p');
+   // p.innerHTML = sub;
+    
+    //p.innerHTML = sub;
+    let p = document.querySelector(`#title${ii}`);
+    p.classList.toggle("through");
+
+    localStorage.setItem("through", JSON.stringify(p.classList.value));
+    let localthr = JSON.parse(localStorage.getItem("through"));
+    console.log(localthr);
+    let localtrh = p.classList.value;
+    if(localtrh === "through"){
+        localStorage.setItem("through", JSON.stringify(p.classList.value));
+        p.classList.value = localtrh;
+        console.log(p)
+    }else{
+        localStorage.setItem("through", JSON.stringify(p.classList.value));
+        p.classList.value = '';
+        console.log(p)
+    }
+
+
+    
+    /*let tasks = JSON.parse(localStorage.getItem(`tasks`));
+    for(let i = 0; i < tasks.length; i++){
+        if(tasks[i] == ii){
+            //tasks[i].classList.toggle("through");
+            //tasks[i].style.background = "red";
+            console.log(ii);
+            let p = ii;
+            let a = JSON.stringify(p);
+            console.log(a);
+            p.style.cssText = 'background: red;';
+        };
+    };*/
+};
 
 
 
